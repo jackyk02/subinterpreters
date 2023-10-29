e296b018fe9d5dd75cd17226ae1458851949cd5b
jackyk02 <jackykwok@berkeley.edu>
2023-10-28 21:45:56 -0700

    remove python count decrement

--- lib/python_port.c
+++ lib/python_port.c
@@ -48,7 +48,7 @@ PyTypeObject py_port_capsule_t;
  * @param py_object A PyObject with count 1 or greater.
  */
 void python_count_decrement(void* py_object) {
-    Py_XDECREF((PyObject*)py_object);
+    //Py_XDECREF((PyObject*)py_object);
 }
 
 //////////// set Function(s) /////////////
